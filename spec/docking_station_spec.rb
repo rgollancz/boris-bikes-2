8f810ece6a84420548258beebf2c5dffdf892d07
Rosie <r.gollancz@gmail.com>
2016-09-28 16:56:12 +0100

    Add error raising and test when no bikes available. Remove bike creation from release method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,17 @@ require 'docking_station'
 
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
-    it "releases a working bike" do
-      bike = subject.release_bike
-      expect(bike).to be_working
+
+    describe '#release_bike' do
+      it "releases a working bike" do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+      end
     end
+
     it { is_expected.to respond_to(:dock).with(1).argument }
+
     it { is_expected.to respond_to(:bike) }
 
     it "docks a bike" do
@@ -19,4 +25,11 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.bike).to eq bike
     end
+
+    describe '#release_bike' do
+      it "raises error when no bikes are docked" do
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
+    end
+    
 end
