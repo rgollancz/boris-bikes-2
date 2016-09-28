88f65c2c7f098c1406758f0313f852f7a73d4a97
eunkenlow <eunkenlow@gmail.com>
2016-09-28 15:21:17 +0100

    added dock method and bike attribute

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,17 @@ describe DockingStation do
       bike = subject.release_bike
       expect(bike).to be_working
     end
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    it { is_expected.to respond_to(:bike) }
+
+    it "docks a bike" do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+    end
+
+    it "returns docked bikes" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
 end
